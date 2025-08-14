103814cca560c359670bb71c284072add8b50ec6
Victor Narazas <victornarazas2005@gmail.com>
2025-08-14 00:27:33 -0500

    Crud Proveedor Finish

---
+++ ApiToolify/Data/Repositorios/ProdRepo.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Data.SqlClient;
+using ProyectoDSWToolify.Data.Contratos;
+using ProyectoDSWToolify.Models;
+
+namespace ProyectoDSWToolify.Data.Repositorios
+{
+    public class ProdRepo : ICrud<Producto>
+    {
+        private readonly IConfiguration config;
+        private readonly string cadenaConexion;
+
+        public ProdRepo(IConfiguration configuration)
+        {
+
+            config = configuration;
+            cadenaConexion = config["ConnectionStrings:DB"];
+        }
+
+        public Producto Actualizar(string tipo, Producto entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Eliminar(string tipo, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> ListaCompleta()
+        {
+            List<Producto> listaProductos = new List<Producto>();
+            using (SqlConnection con = new SqlConnection(cadenaConexion))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("listarProductos", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    using (SqlDataReader r = cmd.ExecuteReader())
+                    {
+                        if (r != null && r.HasRows)
+                        {
+                            while (r.Read())
+                            {
+                                
+                                listaProductos.Add(new Producto()
+                                {
+                                    idProducto = r.IsDBNull(0) ? 0 : r.GetInt32(0),
+
+                                    nombre = r.IsDBNull(1) ? "" : r.GetString(1),
+                                    descripcion = r.IsDBNull(2) ? "" : r.GetString(2),
+                                    proveedor = new Proveedor()
+                                    {
+                                        razonSocial = r.IsDBNull(3) ? "" : r.GetString(3),
+                                    },
+                                    categoria = new Categoria()
+                                    {
+                                        descripcion = r.IsDBNull(4) ? "" : r.GetString(4),
+                                    },
+                                    precio = r.IsDBNull(5) ? 0 : r.GetDecimal(5),
+                                    stock = r.IsDBNull(6) ? 0 : r.GetInt32(6),
+                                });
+                                
+                            }
+                           
+                        }
+                    }
+                }
+            }
+           
+            return listaProductos;
+        }
+
+        public Producto ObtenerId(string tipo, int id)
+        {
+             Producto prodEncontrado = new Producto();
+
+            using (SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                cn.Open();
+                using (SqlCommand cm = new SqlCommand("crudProductos", cn))
+                {
+                    cm.CommandType = System.Data.CommandType.StoredProcedure;
+                    cm.Parameters.AddWithValue("@tipo", tipo);
+                    cm.Parameters.AddWithValue("@idProducto", id);
+
+                    using (SqlDataReader r = cm.ExecuteReader()) {
+                        if (r != null && r.HasRows) {
+                            while (r.Read()) {
+                                prodEncontrado = new Producto() 
+                               {
+                                    idProducto = r.IsDBNull(0) ? 0 : r.GetInt32(0),
+
+                                    nombre = r.IsDBNull(1) ? "" : r.GetString(1),
+                                    descripcion = r.IsDBNull(2) ? "" : r.GetString(2),
+                                    proveedor = new Proveedor()
+                                    {
+                                        razonSocial = r.IsDBNull(3) ? "" : r.GetString(3),
+                                    },
+                                    categoria = new Categoria()
+                                    {
+                                        descripcion = r.IsDBNull(4) ? "" : r.GetString(4),
+                                    },
+                                    precio = r.IsDBNull(5) ? 0 : r.GetDecimal(5),
+                                    stock = r.IsDBNull(6) ? 0 : r.GetInt32(6),
+                                    imagen = r.IsDBNull(7) ? "" : r.GetString(7),
+                                    fechaRegistro = r.IsDBNull(8) ? new DateTime() : r.GetDateTime(8),
+                                    estado = r.IsDBNull(9) ? true : r.GetBoolean(9),
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            return prodEncontrado;  
+        }
+
+        public Producto Registrar(string tipo, Producto producto)
+        {
+            Producto prodGuardodo = new Producto(); 
+            int idRegistrado;
+            using (SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                cn.Open();
+                using (SqlCommand cm = new SqlCommand("crudProductos", cn))
+                {
+                    cm.CommandType = System.Data.CommandType.StoredProcedure;
+                    cm.Parameters.AddWithValue("@tipo", tipo);
+                    cm.Parameters.AddWithValue("@nombre", producto.nombre);
+                    cm.Parameters.AddWithValue("@descripcion", producto.descripcion);
+                    cm.Parameters.AddWithValue("@idProveedor", producto.proveedor.idProveedor);
+                    cm.Parameters.AddWithValue("@idCategoria", producto.categoria.idCategoria);
+                    cm.Parameters.AddWithValue("@precio", producto.precio);
+                    cm.Parameters.AddWithValue("@stock", producto.stock);
+                    cm.Parameters.AddWithValue("@imagen", producto.imagen);
+                    cm.Parameters.AddWithValue("@fecha", producto.fechaRegistro);
+                  
+
+                    idRegistrado = Convert.ToInt32(cm.ExecuteScalar());
+                }
+               
+
+            }
+            prodGuardodo = producto;
+
+            return prodGuardodo;
+        }
+    }
+}
--- BaseDeDatos/ScriptToolify.sql
+++ BaseDeDatos/ScriptToolify.sql
@@ -11,6 +11,19 @@ begin
 end
 go
 
+create or alter proc sp_Distrito
+@tipo varchar(50),
+@id int
+as
+begin
+	if @tipo= 'distritoId'
+	begin
+	select ID_DISTRITO,NOMBRE from TB_DISTRITO where ID_DISTRITO = @id
+	end
+end
+go
+
+
 create or alter proc crudProveedores
 @tipo varchar(50),
 @idProveedor int = 0,
@@ -40,8 +53,10 @@ begin
 	
 	if @tipo = 'detalle'
 		begin
-			select ID_PROVEEDOR, RUC, RAZON_SOCIAL, TELEFONO, DIRECCION, ID_DISTRITO, FECHA_REGISTRO, ESTADO
-			from TB_PROVEEDOR where ID_PROVEEDOR = @idProveedor
+			select ID_PROVEEDOR, RUC, RAZON_SOCIAL, TELEFONO, DIRECCION,d.ID_DISTRITO ,d.NOMBRE, FECHA_REGISTRO, ESTADO
+			from TB_PROVEEDOR p
+			Inner join TB_DISTRITO d on p.ID_DISTRITO = d.ID_DISTRITO 
+			where ID_PROVEEDOR = @idProveedor
 		end
 
 	if @tipo = 'desactivar'
@@ -50,5 +65,72 @@ begin
 			set ESTADO = 0 where ID_PROVEEDOR = @idProveedor
 		end
 end
+GO
+select * from TB_DISTRITO
+go
 
-select * from TB_PROVEEDOR
+select * From TB_PROVEEDOR where ID_PROVEEDOR = 21
+go
+
+--SCRIPTS PRODUCTOS
+create or alter proc listarProductos
+as 
+begin
+	select pr.ID_PRODUCTO, pr.NOMBRE, pr.DESCRIPCION, pv.RAZON_SOCIAL, ct.DESCRIPCION, pr.PRECIO, pr.STOCK from TB_PRODUCTO pr 
+	INNER JOIN TB_CATEGORIA ct ON pr.ID_CATEGORIA = ct.ID_CATEGORIA
+	INNER JOIN TB_PROVEEDOR pv ON pr.ID_PROVEEDOR = pv.ID_PROVEEDOR
+end
+go
+
+create or alter proc crudProductos
+@tipo varchar(50) = '',
+@idProducto int = 0,
+@nombre varchar(50) = '',
+@descripcion varchar(100) = '',
+@idProveedor int = 0,
+@idCategoria int = 0,
+@precio decimal(10,2) =0.0,
+@stock int = 0,
+@imagen varchar(100) = '',
+@fecha datetime = null,
+@estado bit = 1  
+as
+begin
+	
+	if @tipo = 'registrar'
+		begin
+			insert into TB_PRODUCTO(NOMBRE, DESCRIPCION, ID_PROVEEDOR, ID_CATEGORIA, PRECIO, STOCK, IMAGEN, FECHA_REGISTRO, ESTADO)
+			values(@nombre,@descripcion,@idProveedor,@idCategoria,@precio,@stock,@imagen,@fecha,1)
+			select @@IDENTITY
+		end	
+	
+	if @tipo = 'actualizar'
+		begin
+			if @imagen = 'null'
+				update TB_PRODUCTO 
+					set NOMBRE = @nombre, DESCRIPCION = @descripcion,ID_PROVEEDOR=@idProveedor,ID_CATEGORIA = @idCategoria,
+					PRECIO = @precio, STOCK = @stock, FECHA_REGISTRO = @fecha, ESTADO = 1 where ID_PRODUCTO = @idProducto
+			else
+				update TB_PRODUCTO 
+					set NOMBRE = @nombre, DESCRIPCION = @descripcion,ID_PROVEEDOR=@idProveedor,ID_CATEGORIA = @idCategoria,
+					PRECIO = @precio, STOCK = @stock,IMAGEN = @imagen, FECHA_REGISTRO = @fecha, ESTADO = 1 where ID_PRODUCTO = @idProducto
+		end
+	if @tipo = 'desactivar'
+		begin
+			update TB_PRODUCTO set ESTADO = 0 where ID_PRODUCTO = @idProducto
+		end
+
+	if @tipo = 'detalle'
+		begin
+			select pr.ID_PRODUCTO, pr.NOMBRE, pr.DESCRIPCION, pv.RAZON_SOCIAL, ct.DESCRIPCION, pr.PRECIO, pr.STOCK, pr.IMAGEN,
+				   pr.FECHA_REGISTRO, pr.ESTADO		
+			from TB_PRODUCTO pr 
+			INNER JOIN TB_CATEGORIA ct ON pr.ID_CATEGORIA = ct.ID_CATEGORIA
+			INNER JOIN TB_PROVEEDOR pv ON pr.ID_PROVEEDOR = pv.ID_PROVEEDOR where ID_PRODUCTO = @idProducto
+		end
+end
+go
+
+exec crudProductos 'detalle',54
+
+select * from  TB_PRODUCTO
