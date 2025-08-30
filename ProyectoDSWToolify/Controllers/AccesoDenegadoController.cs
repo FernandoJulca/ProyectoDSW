ce228ef4f12704cd973c501fa1215f066d2ff625
Victor Narazas <victornarazas2005@gmail.com>
2025-08-30 13:12:43 -0500

    Agregando ERROR 403

---
+++ ProyectoDSWToolify/Controllers/AccesoDenegadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDSWToolify.Controllers
+{
+    public class AccesoDenegadoController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
